c9c80e6e136a5ab2cb13e3949be15eb45da6e0ca
nnolmos1 <nnolmos1@gmail.com>
2026-08-03 00:25:18 -0500

    Updated files

---
+++ Components/Info.jsx
@@ -0,0 +1,25 @@
+export default function Info() {
+    return (
+        <div className="info">
+            <img src="./headshot_2026.png" alt="headshot" className="info-img" />
+
+            <div className="info-text">
+                <h1> Noah Olmos</h1>
+                <h2> Frontend Developer & Designer </h2>
+                <p> noaholmos.website(wip) </p>
+            </div>
+
+            <div className="info-buttons">
+                <div className="email-button"> 
+                    <img src="./Images/email-icon.png" alt="email icon" className="email-icon" />
+                    <button> Email </button>
+                </div>
+
+                <div className="linkedin-button">
+                    <img src="./Images/linkedin-icon.png" alt="linkedin icon" className="linkedin-icon" />
+                    <button> LinkedIn </button>
+                </div>o
+            </div>
+        </div>
+    )
+}
